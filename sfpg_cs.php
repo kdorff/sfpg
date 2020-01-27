aaf771c2cb0f8af7e55aaea2ae4189d1e66fe797
Jiří Setnička <setnicka@seznam.cz>
2020-01-27 12:55:16 +0100

    Czech translation

---
+++ sfpg_cs.php
@@ -0,0 +1,60 @@
+<?php
+
+define('TEXT_GALLERY_NAME', 'Single File PHP Gallery');
+define('TEXT_HOME', 'Galerie');
+define('TEXT_CLOSE_IMG_VIEW', 'Zavřít obrázek');
+define('TEXT_ACTUAL_SIZE', 'Skutečná velikost');
+define('TEXT_PREVIOUS', '&#x25C4;&#xFE0E; Předchozí');
+define('TEXT_NEXT', 'Následující &#x25BA;&#xFE0E;');
+define('TEXT_INFO', 'Informace');
+define('TEXT_INFO_LABEL', 'Informace');
+define('TEXT_DOWNLOAD', 'Stáhnout v plném rozlišení');
+define('TEXT_SLIDESHOW', 'Slideshow');
+define('TEXT_NO_IMAGES', 'Žádné obrázky v galerii');
+define('TEXT_DATE', 'Datum');
+define('TEXT_FILESIZE', 'Velikost');
+define('TEXT_IMAGESIZE', 'Rozlišení');
+define('TEXT_DIR_NAME', 'Název galerie');
+define('TEXT_IMAGE_NAME', 'Název obrázku');
+define('TEXT_IMAGE_DESCRIPTION', 'Popis obrázku');
+define('TEXT_FILE_NAME', 'Jméno souboru');
+define('TEXT_DIRS', 'Subgalerie');
+define('TEXT_IMAGES', 'Obrázky');
+define('TEXT_IMAGE_NUMBER', 'Číslo obrázku');
+define('TEXT_FILES', 'Soubory');
+define('TEXT_DESCRIPTION', 'Podrobnosti');
+define('TEXT_DIRECT_LINK_GALLERY', 'Trvalý odkaz na galerii');
+define('TEXT_DIRECT_LINK_IMAGE', 'Trvalý odkaz na obrázek');
+define('TEXT_NO_PREVIEW_FILE', 'Bez náhledu');
+define('TEXT_IMAGE_LOADING', 'Nahrávám… ');
+define('TEXT_LINKS', 'Odkazy');
+define('TEXT_NOT_SCALED', '--');
+define('TEXT_LINK_BACK', 'Zpět na stránku');
+define('TEXT_SCALED_TO', 'Zmenšeno na');
+define('TEXT_YES', 'Ano');
+define('TEXT_NO', 'Ne');
+
+define('TEXT_ZIP_ROOT_NAME', 'Single File PHP Gallery');
+define('TEXT_ZIP_NOTHING', 'Nic k zazipování');
+define('TEXT_ZIP_DL', 'Stáhnout všechny obrázky jako ZIP-soubor:');
+define('TEXT_ZIP_BUTTON', 'Vygenerovat ZIP-soubor a stáhnout');
+define('TEXT_ZIP_BUTTON_LIVE', 'Stáhnout celou galerii');
+define('TEXT_ZIP_WAIT', 'Vyrábím ZIP, prosím počkejte…');
+
+define('TEXT_EXIF', 'EXIF informace');
+define('TEXT_EXIF_DATE', 'EXIF datum');
+define('TEXT_EXIF_CAMERA', 'Fotoaparát');
+define('TEXT_EXIF_ISO', 'ISO');
+define('TEXT_EXIF_SHUTTER', 'Čas');
+define('TEXT_EXIF_APERTURE', 'Světelnost');
+define('TEXT_EXIF_FOCAL', 'Ohnisková vzdálenost');
+define('TEXT_EXIF_FLASH', 'Blesk');
+define('TEXT_EXIF_LATITUDE', 'Zeměpisná šířka');
+define('TEXT_EXIF_LONGITUDE', 'Zeměpisná délka');
+define('TEXT_EXIF_MAP', 'Zobrazit na Google mapách');
+define('TEXT_EXIF_MAP_EMBED', 'Mapa');
+
+define('TEXT_IPTC', 'IPTC informace');
+
+define('TEXT_GALLERY_DOWNLOAD', 'Stáhnout celou galerii (všechny fotky)');
+include('sfpg.php');
